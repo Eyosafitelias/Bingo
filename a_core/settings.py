c93a778dae802ec7298f292ecb05406365674667
Eyosafitelias <eyosafit90@gmail.com>
2025-05-29 14:33:28 +0300

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -154,6 +154,11 @@ CHANNEL_LAYERS = {
     },
 }
 
+if not DEBUG:
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
