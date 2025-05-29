5c6fb8820c71e74177e11fad5a4698450eec0b8f
Eyosafitelias <eyosafit90@gmail.com>
2025-05-29 14:15:05 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Exit on any error
 set -o errexit
 
 # Run migrations
 python manage.py migrate
 
-# Start the server
+# Start the bot in the background
+python manage.py runbot &
+
+# Start the main server
 gunicorn a_core.wsgi:application --bind 0.0.0.0:$PORT
